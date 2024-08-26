69ae4ba7cd7368cdc0ef13e897c118b9f5405819
Lenon Mauer <lenonmauer0@gmail.com>
2024-08-26 13:12:25 -0300

    fix response return

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
-  Response.json({ ok: true, time: Date.now() })
+  return Response.json({ ok: true, time: Date.now() })
 }
 
