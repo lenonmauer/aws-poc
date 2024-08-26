9a8f15691b931e58d9dcf14fb818d150cbb64165
Lenon Mauer <lenonmauer0@gmail.com>
2024-08-26 12:46:54 -0300

    hello api

---
+++ src/app/api/hello/router.ts
@@ -0,0 +1,3 @@
+export async function POST(request: Request) {
+  Response.json({ ok: true })
+}
--- src/app/hello/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Hello() {
-  return (
-    <main>
-      <p>Hello page</p>
-    </main>
-  );
-}
